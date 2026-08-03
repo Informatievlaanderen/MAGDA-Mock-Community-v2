03c6d009d0f5490d7598ec8fb9e31c3526f98ae8
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2026-08-03 11:24:53 +0200

    fix: read-version script

--- read_version.sh
+++ read_version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
 
-base_version=$(cat Dockerfile | grep "^FROM" | cut -d':' -f2)
+# base_version=$(cat Dockerfile | grep "^FROM" | cut -d':' -f2)
+base_version=$(cat Dockerfile | grep "^ARG MAGDAMOCK_VERSION" | cut -d'=' -f2 | tr -d "\"")
 community_version=$(cat version.txt)
 
 echo "$base_version-$community_version"
